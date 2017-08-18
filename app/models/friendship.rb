a244ded12b29af1b370e8e8bdb256f855475ed95
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-08-18 18:07:26 -0400

    added friendship model

---
+++ app/models/friendship.rb
@@ -0,0 +1,15 @@
+class Friendship < ApplicationRecord
+  after_create :create_inverse_relationship
+  belongs_to :user
+  belongs_to :friend, class_name: 'User', foreign_key: "friend_id"
+
+  validates :user, presence: true
+  validates :friend, presence: true, uniqueness: { scope: :user }
+
+  private
+
+  def create_inverse_relationship
+    friend.friendships.create(friend: user)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_many :reviews
-  has_many :restaurants, through: :reviews
   has_many :user_restaurants
   has_many :restaurants, through: :user_restaurants
+  has_many :friendships
+  has_many :friends, through: :friendships
 
   has_secure_password
-  validates :username, uniqueness:true, presence:true
+  validates :username, uniqueness: true
 end
